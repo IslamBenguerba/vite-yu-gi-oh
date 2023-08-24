c9cf6ad8ae45d35e92974f90f730ed24776fca68
Islam.Benguerba <islam.benguerba@gmail.com>
2023-08-24 20:47:18 +0200

    aggiunta la ricerca

--- vite-yu-gi-oh/src/components/CardContainer.vue
+++ vite-yu-gi-oh/src/components/CardContainer.vue
@@ -1,35 +1,55 @@
 <script>
 import axios from 'axios'
+import cardInterest from './Navbar.vue'
 export default {
     data() {
         return {
-            listaCard: []
+            listaCard: [],
+            cardInterest: ''
         }
     },
     methods: {
         importcard() {
             axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then((response) => {
-                this.listaCard = response.data
-                console.log(this.listaCard)
+                this.listaCard = response.data.data
                 
-                for(let i = 0; i<this.listaCard.length;i++){
+
+                for (let i = 0; i < this.listaCard.length; i++) {
                     const card = this.listaCard[i]
-                    console.log(card)
+                    if (typeof card.archetype === 'undefined') {
+                        
+                        if (typeof card.atk === 'undefined') {
+                            card.archetype = 'Magic'
+                        } else {
+                            card.archetype = 'Monster'
+                        }
+
                     }
-                
-                
-            })}
+                }
+            })
+        },
+        filterlist() {
+            this.listaCard = []
+            axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0&fname='+this.cardInterest+'%').then((response) => {
+                this.listaCard =response.data.data
+                console.log(response.data)
+                console.log(this.listaCard)
+            })
+            console.log(this.cardInterest)
+        }
     },
     mounted() {
         this.importcard()
-        
+
     }
 }
 </script>
 <template>
+    <input type="text" class="input" v-model="cardInterest">
+    <button class="btn btn-primary" @click="filterlist()">cerca</button>
     <div class="container">
         <div class=" row container-card">
-            <div class="col col-card" v-for="card in listaCard.data" >
+            <div class="col col-card" v-for="card in listaCard">
                 <div class="card">
                     <img class="card-img-top" :src="card['card_images'][0].image_url">
                     <div class="card-body">
